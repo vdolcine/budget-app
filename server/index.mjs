76f60de0b9ea71e53cf06c74facdb118901e2322
Vastille Dolcine <vdolcine@gmail.com>
2023-02-16 10:18:30 -0800

    Initial Server Setup
    Installed Express and initialized server file.

---
+++ server/index.mjs
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const PORT = 3001;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ message: 'hello from the server' });
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost: ${PORT}`);
+})
