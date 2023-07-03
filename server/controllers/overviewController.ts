9a102c2ead779e771b76edd35f730a79103c5ab8
Vastille <vdolcine@gmail.com>
2023-07-03 15:43:27 -0700

    Use global error handler in the controller functions

--- server/controllers/overviewController.ts
+++ server/controllers/overviewController.ts
@@ -14,9 +14,9 @@ const overviewController: overviewControllerType = {
     const queryText = `SELECT * FROM overview_june_2023 o JOIN categories c ON c.category_id = o.category_id`;
     db.sqlquery(queryText, undefined, (err: any, result: any) => {
       if (!result) {
-        res.send(
-          `ERROR: Could not retrieve data from overviewController.getBudet`
-        );
+        next({
+          log: `ERROR: Could not retrieve data from overviewController.getBudet`,
+        });
       }
       res.locals.overview = result.rows;
       next();
@@ -26,9 +26,9 @@ const overviewController: overviewControllerType = {
     const queryText = `SELECT category_id, sum(item_value) FROM expenses_june_2023 GROUP BY category_id`;
     db.sqlquery(queryText, undefined, (err: any, result: any) => {
       if (!result) {
-        res.send(
-          `ERROR: Could not retrieve data from overviewController.getActual`
-        );
+        next({
+          log: `ERROR: Could not retrieve data from overviewController.getActual`,
+        });
       }
       res.locals.actual = result.rows;
       next();
@@ -43,9 +43,9 @@ const overviewController: overviewControllerType = {
       queryParamsCategory,
       (err: any, result: any) => {
         if (!result) {
-          res.send(
-            `ERROR: Could not post category from overviewController.postCategory`
-          );
+          next({
+            log: `ERROR: Could not post category from overviewController.postCategory`,
+          });
         }
         res.locals.category_id = result.rows[0].category_id;
         next();
@@ -58,9 +58,9 @@ const overviewController: overviewControllerType = {
     const queryTextBudget = `INSERT INTO overview_june_2023 (total_category, category_id) VALUES ($1, $2) RETURNING *`;
     db.sqlquery(queryTextBudget, queryParamsBudget, (err: any, result: any) => {
       if (!result) {
-        res.send(
-          `ERROR: Could not post budget from overviewController.postCategory `
-        );
+        next({
+          log: `ERROR: Could not post budget from overviewController.postCategory `,
+        });
       }
       // console.log(`Result Budget: `, result.rows);
       next();
@@ -70,9 +70,9 @@ const overviewController: overviewControllerType = {
     const queryText = ``;
     db.sqlquery(queryText, undefined, (err: any, result: any) => {
       if (!result) {
-        res.send(
-          `ERROR: Could not retrieve data from overviewController.getBudet`
-        );
+        next({
+          log: `ERROR: Could not retrieve data from overviewController.getBudet`,
+        });
       }
       res.locals.overview = result.rows;
       next();
